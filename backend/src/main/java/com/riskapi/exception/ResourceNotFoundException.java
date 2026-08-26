a22918443c3f91a5ee57fda93c6e57261c41ba7f
Tejaswi Koppula <tejaswik4646@gmail.com>
2026-08-26 12:42:23 -0400

    Add ResourceNotFoundException class

---
+++ backend/src/main/java/com/riskapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.riskapi.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
