5ef997bf6ff75f8a0f09ce519dd93444ceeff039
Tejaswi Koppula <tejaswik4646@gmail.com>
2026-08-26 12:36:01 -0400

    Add entity models

---
+++ backend/src/main/java/com/riskapi/model/User.java
@@ -0,0 +1,19 @@
+package com.riskapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password; // BCrypt hashed
+}
