d4eee7a6445cba080d7deb975049be2408677c79
Tejaswi Koppula <tejaswik4646@gmail.com>
2026-08-26 12:31:16 -0400

    dd main application class

---
+++ backend/src/main/java/com/riskapi/LoanRiskApiApplication.java
@@ -0,0 +1,11 @@
+package com.riskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanRiskApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoanRiskApiApplication.class, args);
+    }
+}
